a7d54aaff3dfe7d2d3455d07e2893246b338f995
Phirun_Oem <phirunoemark123@gmail.com>
2022-06-20 09:43:11 +0700

    added some text

--- facebook_service(frontend)/pages/index.js
+++ facebook_service(frontend)/pages/index.js
@@ -5,7 +5,13 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div >
-      <h2>hello this is </h2>
+      <h2 style={{
+        position:'absolute',
+        top:'50%',
+        left:'50%',
+        transform:'translate(-50%,-50%)'
+        }}>This is project involves face book service!
+      </h2>
     </div>
   )
 }
